e1a27435ba995b014d68fab95b79620992f5522f
John Gikuma <48711420+johnsmaina17@users.noreply.github.com>
2023-08-06 11:43:48 +0300

    Update SmsNofifier.java

--- SmsNofifier.java
+++ SmsNofifier.java
@@ -1,6 +1,6 @@
 public class SmsNofifier implements  Notification{
     @Override
     public void notifyUser() {
-        System.out.println("Sending  sms notification to user, testing jenkins 3");
+        System.out.println("Sending  sms notification to user, testing jenkins 4");
     }
 }
